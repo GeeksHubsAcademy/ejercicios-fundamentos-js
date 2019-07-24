a13a10c94c7c4630b38d3af1da12769f85be923d
David Alfageme <alfalfageme@gmail.com>
2019-07-24 16:55:57 +0200

    Update 3-number.js

--- 7-Prototypes/3-number.js
+++ 7-Prototypes/3-number.js
@@ -1,7 +1,7 @@
 // Adapta la funcion getPrecioMostrar para que devuelva una cadena de texto con formato precio con dos decimales
 // Para 2 debería devolver 2.00 €
 
-// si la función no recibe una cadena de texto o está vacía devolver 'no es un formato correcto'
+// si la función no recibe un número debería devolver devolver 'no es un formato correcto'
 
 let getPrecioMostrar = (precio) => {
     // Aquí tu código.  Desde aquí:
